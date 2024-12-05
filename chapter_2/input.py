b8935dc0397f4f0bae6c1d39c3d817ec8b33d04d
PrasiddhaKhadka <prasiddha@thefireflytech.com>
2024-12-05 08:12:46 +0545

    Add: Chapter 2 Basics

---
+++ chapter_2/input.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+c = input("Enter your name: ")
+
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
---
+++ chapter_2/type_casting.py
@@ -0,0 +1,5 @@
+a = "31.2"
+b = float(a) # a is converted to float (conversion from string to float)
+t = type(b) # to find the type of B variable
+
+print(t)
