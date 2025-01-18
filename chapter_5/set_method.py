8349a86eb89342aa70671537ea42e7d8af0c571e
PrasiddhaKhadka <prasiddha@thefireflytech.com>
2025-01-18 13:18:54 +0545

    Add:Set methods

---
+++ chapter_5/set_method.py
@@ -0,0 +1,11 @@
+s = {
+    1,2,3,4,5,5,6,7,8,9,"Prasid"
+}
+
+print(s,type(s),len(s))
+s.add(10)
+s.remove(10)
+print(s)
+
+#methods of set
+#difference, difference_update, intersection, intersection_update, union,
